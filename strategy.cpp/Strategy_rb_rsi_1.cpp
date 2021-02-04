dcc94f8e226258f1af3749bdfcad4807ad3e2de9
Jent <JentChang@163.com>
2021-02-04 12:05:08 +0800

    Indicators由注册更改为构造函数构造

--- strategy.cpp/Strategy_rb_rsi_1.cpp
+++ strategy.cpp/Strategy_rb_rsi_1.cpp
@@ -2,8 +2,8 @@
 
 
 
-Strategy_rb_rsi::Strategy_rb_rsi(TDEvent* tdevent, Indicators*indn):
-	StrategyTemplate(tdevent, indn)
+Strategy_rb_rsi::Strategy_rb_rsi(TDEvent* tdevent):
+	StrategyTemplate(tdevent)
 {
 	
 }
@@ -14,11 +14,31 @@ Strategy_rb_rsi::~Strategy_rb_rsi()
 
 void Strategy_rb_rsi::InIt()
 {
+	this->__InstrumentID = "rb2105";
+	this->indn->fre(10);
 }
 
 void Strategy_rb_rsi::ReceiveTick(TickInfomation tick)
 {
 	StrategyTemplate::__tick = tick;
 	this->indn->make_bar(tick);
+
+	std::cout << tick.datetime << " "
+		<< tick.InstrumentID
+		<< endl;
+
 	BarInfomation* bar = this->indn->bar();
+	if (this->indn->barclose())
+	{
+		this->OnBar(bar);
+	}
+
+}
+
+void Strategy_rb_rsi::OnBar(BarInfomation * bar)
+{
+	std::cout << bar->TradingDay << " "
+		<< bar->UpdateTime << " "
+		<< bar->InstrumentID
+		<< endl;
 }
--- strategy.cpp/Strategy_rb_rsi_1.h
+++ strategy.cpp/Strategy_rb_rsi_1.h
@@ -11,14 +11,16 @@ class Strategy_rb_rsi:
 	public StrategyTemplate
 {
 public:
-	Strategy_rb_rsi(TDEvent*, Indicators*);
+	Strategy_rb_rsi(TDEvent*);
 	~Strategy_rb_rsi();
 
 public:
 	void InIt();
 	void ReceiveTick(TickInfomation);
+	void OnBar(BarInfomation* bar);
 
 private:
+	
 
 
 };
